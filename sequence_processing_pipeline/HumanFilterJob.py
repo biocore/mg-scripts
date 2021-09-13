e8ae1091233aa56f116fd5422f906db7f06a0108
Charles Cowart <ccowart@ucsd.edu>
2021-09-12 23:13:08 -0700

    Modifications to HumanFilterJob

--- sequence_processing_pipeline/HumanFilterJob.py
+++ sequence_processing_pipeline/HumanFilterJob.py
@@ -1,227 +1,128 @@
 import logging
-import os
-from sequence_processing_pipeline.Job import Job
+from sequence_processing_pipeline.TorqueJob import TorqueJob
+from os.path import join, basename, dirname, abspath
+from os import makedirs
+import re
 
 
-class HumanFilterJob(Job):
-    def __init__(self, SequenceDirectoryObject, nprocs, job_owner_home,
-                 email_list, chemistry, output_dir, final_output_dir):
-        self.sdo = SequenceDirectoryObject
-        self.seq_dir = self.sdo.sequence_directory
-        self.csv_files = []
-        self.nprocs = nprocs
-        self.home = job_owner_home
-        self.email_list = email_list
-        self.chemistry = chemistry
-        self.output_dir = output_dir
-        self.final_output_dir = final_output_dir
+class HumanFilterJob(TorqueJob):
+    def __init__(self, root_dir, project, sample_sheet_path, output_directory, forward, reverse):
         super().__init__()
+        logging.debug("HumanFilterJob Constructor called")
+        self.root_dir = abspath(root_dir)
+        self.project = project
+        self.job_script_path = join(self.root_dir, 'Data', 'Fastq', project, 'fastqc_qsub.sh')
+        self.job_name = "%s_%s_%s" % (project, forward, reverse)
+        self.nprocs = 16
+        self.sample_sheet_path = sample_sheet_path
+        self.stdout_log_path = join(dirname(self.job_script_path), 'something_%s_%s.out.log' % forward, reverse)
+        self.stderr_log_path = join(dirname(self.job_script_path), 'something_%s_%s.err.log' % forward, reverse)
+        self.output_dir_path = abspath(output_directory)
+        self.bcl2fastq_bin_path = "~/bcl2fastq_2_20/bin/bcl2fastq"
+        makedirs(dirname(self.job_script_path), exist_ok=True)
+        makedirs(dirname(self.output_dir_path), exist_ok=True)
 
-    def _get_run_config_info(self, sample_sheet_path):
-        with open(sample_sheet_path, 'r') as f:
-            # we can (and should) open up this file as a proper INI
-            # file. However it may have trailing ',' characters,
-            # which may impede this. Hence, open it as a regular
-            # file and clean it up before processing.
-            lines = f.readlines()
-            # first, let's strip out \n and \r\n and any leading or
-            # trailing whitespaces
-            lines = [x.strip() for x in lines]
-            # second, let's strip out trailing ',' characters, if
-            # they're present. We'll keep any leading ',' characters
-            # or in-line ',' characters.
-            lines = [x.rstrip(',') for x in lines]
-            # lastly, there have been cases where lines contain only
-            # ',' characters. Hence, look for and remove these now
-            # empty lines before continuing.
-            lines = [x for x in lines if x]
-            # since the file is already in memory, we won't use INI
-            # library to parse it, (for now). Although it would be
-            # cleaner.
-            metadata = []
-            sentinel = False
-            for i in range(0, len(lines)):
-                if lines[i] == '[Bioinformatics]':
-                    # when Bioinformatics section is found, start
-                    # copying lines to the list buffer, but don't
-                    # copy the header itself.
-                    sentinel = True
-                elif lines[i].startswith('['):
-                    # when the next header is found, stop copying
-                    # lines to the list buffer. Don't include this
-                    # header line, either.
-                    sentinel = False
-                elif sentinel is True:
-                    # this should be a line in between
-                    # [Bioinformatics] and the next section. Copy it
-                    # to the buffer.
-                    metadata.append(lines[i])
-                # if the end of the file is reached before the next
-                # header is found, that means there were no more
-                # headers and that's okay too.
-
-            # remove duplicate lines (this appears to be an issue in
-            # the original bash scripts.) and sort.
-            metadata = list(set(metadata))
-            metadata.sort()
-
-            # before returning, split each line into a list, so it's
-            # easier to iterate through each line of values.
-            return [x.split(',') for x in metadata]
-
-    def _split_list(self, original_list, lines_per_file, file_prefix,
-                    file_extension, destination_path):
-        def _chunk_list(some_list, n):
-            # taken from https://bit.ly/38S9O8Z
-            for i in range(0, len(some_list), n):
-                yield some_list[i:i + n]
-
-        new_files = []
-        count = 0
-        for chunk in _chunk_list(original_list, lines_per_file):
-            # create the filename for the new split file
-            s = '%s%d.%s' % (file_prefix, count, file_extension)
-            # prepend the path of the original file to the new split files
-            s = os.path.join(destination_path, s)
-            with open(s, 'w') as f2:
-                for line in chunk:
-                    f2.write(line)
-            new_files.append(s)
-            count += 1
-
-        return new_files
-
-    def create_qsub_file(self, seq_dir, project, file_base, home_dir,
-                         email_list, trim_file, slurm_array_task_id,
-                         chemistry, output_dir, adapter_a, adapter_A,
-                         a_trim, h_filter, qiita_proj, final_output_dir):
+    def _make_job_script(self):
         lines = []
+
         lines.append("#!/bin/bash")
-        lines.append("#SBATCH --job-name={}_%A_%a".format(project))
-        lines.append("#SBATCH --ntasks=%d" % self.nprocs)
-        lines.append("#SBATCH --ntasks-per-node=%d" % self.nprocs)
-        lines.append("#SBATCH --export=ALL")
-        # assuming for now that 72 hours for all jobs is appropriate
-        lines.append("#SBATCH --time=72:00:00")
-        # assuming for now that 6G is proper for all jobs.
-        lines.append("#SBATCH --mem-per-cpu=6G")
-        s = "#SBATCH --output={}/filter_jobs/%x_%A_%a.out".format(home_dir)
-        lines.append(s)
-        s = "#SBATCH --error={}/filter_jobs/%x_%A_%a.err".format(home_dir)
-        lines.append(s)
-        lines.append("#SBATCH --mail-type=ALL")
-        lines.append("#SBATCH --mail-user=%s" % ','.join(email_list))
-        # assume for now this hardcoded path is acceptable
-        lines.append("source ~/miniconda3/bin/activate test_env_2")
-        # assume for now the backslash below is intentional
-        lines.append("file={}\\{}".format(trim_file, slurm_array_task_id))
-        lines.append(
-            'cmd="sh ~/seq_proc_dev/fpmmp.sh -d %s -D /Data/Fastq -S %s\%s -p %s -C %s -c %d -o %s -O %s -a %s -A %s -g %s -G %s -q %s -f %s"' % (
-            seq_dir, trim_file, slurm_array_task_id, project, chemistry, self.nprocs, output_dir,
-            os.path.basename(seq_dir), adapter_a, adapter_A, a_trim, h_filter, qiita_proj, final_output_dir))
-        lines.append('echo "$cmd"')
-        lines.append("date")
-        lines.append("echo 'Executing: \"$cmd\"'")
-        lines.append('eval "$cmd"')
-        lines.append("date")
+        # The Torque equiv for calling SBATCH on this script and supplying params
+        # w/environment variables is to do the same on QSUB but with -v instead of
+        # --export. The syntax of the values are otherwise the same.
+        # -v <variable[=value][,variable2=value2[,...]]>
 
-        file_path = os.path.join(seq_dir, 'Data', 'Fastq', project,
-                                 f'{file_base}_qsub.sh')
-        if os.path.exists(file_path):
-            logging.error("%s is being overwritten." % file_path)
+        # declare a name for this job to be sample_job
+        lines.append("#PBS -N %s" % self.job_name)
 
-        with open(file_path, 'w') as f:
+        # what torque calls a queue, slurm calls a partition
+        # SBATCH -p SOMETHING -> PBS -q SOMETHING
+        # (Torque doesn't appear to have a quality of service (-q) option. so this
+        # will go unused in translation.)
+        lines.append("#PBS -q long8gb")
+
+        # request one node
+        # Slurm --ntasks-per-node=<count> -> -l ppn=<count>	in Torque
+        lines.append("#PBS -l nodes=1:ppn=%d" % self.nprocs)
+
+        # Slurm --export=ALL -> Torque's -V
+        lines.append("#PBS -V")
+
+        # Slurm walltime limit --time=24:00:00 -> Torque's -l walltime=<hh:mm:ss>
+        # using the larger value found in the two scripts (72 vs ? hours)
+        lines.append("#PBS -l walltime=72:00:00")
+
+        # send email to charlie when a job starts and when it terminates or
+        # aborts. This is used to confirm the package's own reporting
+        # mechanism is reporting correctly.
+        lines.append("#PBS -m bea")
+
+        # specify your email address
+        lines.append("#PBS -M ccowart@ucsd.edu")
+
+        # min mem per CPU: --mem-per-cpu=<memory> -> -l pmem=<limit>
+        # taking the larger of both values (10G > 6G)
+        #lines.append("#PBS -l pmem=10gb")
+        # revisit the mem stuff later.
+
+        # --output -> -o
+        lines.append("#PBS -o %s" % self.stdout_log_path)
+        lines.append("#PBS -e %s" % self.stderr_log_path)
+
+        # there is no equivalent for this in Torque, I believe
+        # --cpus-per-task 1
+
+        # We probably do not need to activate this Python environment, but I
+        # will store it here in comments.
+        # source ~/miniconda3/bin/activate test_env_2
+
+        # By default, PBS scripts execute in your home directory, not the
+        # directory from which they were submitted. Use root_dir instead.
+        lines.append("cd %s"  % self.root_dir)
+        '''
+        MAKE SURE ATRIM AND HFILTER ARE TRUE/False or CONVERTED TO IT
+        MAKE SURE adapter_a and ADAPTER_A are None if they are NA
+                lines.append(
+                    'cmd="sh ~/seq_proc_dev/fpmmp.sh -d %s -D /Data/Fastq -S %s\%s -p %s -C %s -c %d -o %s -O %s -a %s -A %s -g %s -G %s -q %s -f %s"' % (
+                    seq_dir, trim_file, slurm_array_task_id, project, chemistry, self.nprocs, output_dir,
+                    os.path.basename(seq_dir), adapter_a, adapter_A, a_trim, h_filter, qiita_proj, final_output_dir))
+        '''
+        lines.append('cmd="%s --sample-sheet %s --mask-short-adapter-reads \
+                      1 -R . -o %s --loading-threads 8 --processing-threads \
+                      8 --writing-threads 2 --create-fastq-for-index-reads \
+                      --ignore-missing-bcls"' % (self.bcl2fastq_bin_path,
+                                                 self.sample_sheet_path,
+                                                 self.output_dir_path))
+        lines.append("echo $cmd")
+        lines.append("eval $cmd")
+        lines.append("return_code=$?")
+        p = "%s/%s.return_code" % (self.root_dir, self.job_name)
+        lines.append("echo $returncode > %s" % p)
+        lines.append("date >> %s" % p)
+
+        with open(self.job_script_path, 'w') as f:
+            logging.debug("Writing job script to %s" % self.job_script_path)
             for line in lines:
+                # remove long spaces in some lines.
+                line = re.sub('\s+', ' ', line)
                 f.write("%s\n" % line)
 
-    def run(self, sample_sheet_path, seq_dir):
-        # TODO source of this value is very unclear in the original scripts.
-        slurm_array_task_id = 'UNKNOWN'
-        metadata = self._get_run_config_info(sample_sheet_path)
-        fastq_output = os.path.join(seq_dir, 'Data', 'Fastq')
+    def run(self):
+        # we may actually need to make the job script in a sub-dir.
+        self._make_job_script()
+        job_info = self.qsub(self.job_script_path, None, None)
 
-        for read, proj, f_adapter, r_adapter, trim, hfilter, qid in metadata:
-            # extract the numerical component from the project string:
-            # nnnnnn_ddddd
-            proj_check = proj.split('_')[-1]
-            input_count = 0
-            working_dir = os.path.join(fastq_output, proj)
-            for root, dirs, files in os.walk(working_dir):
-                for some_file in files:
-                    if some_file.endswith('.fastq.gz'):
-                        if '_R1_' in some_file:
-                            input_count += 1
+        # if the job returned successfully, we may want to send an
+        # email to the user. If an error occurs, the user will
+        # be notified by the email triggered by PipelineError().
 
-            split_count = 1
-            if input_count > 2000:
-                split_count = 16
-            elif input_count <= 2000 and input_count > 1000:
-                split_count = 10
-            elif input_count <= 1000 and input_count > 500:
-                split_count = 4
 
-            trim_file = 'split_file_'
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
 
-            job_count = split_count + 1
-            file_base = os.path.basename(self.seq_dir)
+    job = HumanFilterJob('./good-bcl-directory',
+                       'THDMI_US_99999',
+                       './good-bcl-directory/good-sample-sheet.csv',
+                       './good-bcl-directory/Data/Fastq/Output')
+    job.run()
 
-            for root, dirs, files in os.walk(working_dir):
-                for some_file in files:
-                    if trim_file in some_file:
-                        some_path = os.path.join(root, some_file)
-                        logging.debug("deleting %s..." % some_path)
-                        os.remove(some_path)
 
-            # we're only supposed to consider the contents of the
-            # working_directory itself, not the sub-directories,
-            # but for now assume that there aren't any sub-dirs.
-            file_base_file_list = []
-            for root, dirs, files in os.walk(working_dir):
-                for some_file in files:
-                    if some_file.endswith('.fastq.gz'):
-                        if '_R1_' in some_file:
-                            file_base_file_list.append(some_file)
-
-            line_count = len(file_base_file_list)
-            lines_per_split = (line_count + split_count - 1) / split_count
-
-            self._split_list(file_base_file_list, lines_per_split, trim_file,
-                             'extension', working_dir)
-
-            # TODO: Modify create_qsub_file to take the member variables
-            #  directly.
-
-            self.create_qsub_file(self.seq_dir,
-                                  proj,
-                                  file_base,
-                                  self.home,
-                                  self.email_list,
-                                  trim_file,
-                                  slurm_array_task_id,
-                                  self.chemistry,
-                                  self.output_dir,
-                                  f_adapter,
-                                  r_adapter,
-                                  trim,
-                                  hfilter,
-                                  qid,
-                                  self.final_output_dir)
-
-            cmd = ['sbatch',
-                   '--qos=seq_proc',
-                   '--parsable',
-                   '--array=0-%d' % int(split_count) - 1,
-                   os.path.join(seq_dir, 'Data', 'Fastq',
-                                proj, file_base + '_qsub.sh')]
-
-            # TODO We may or may not want to wait
-            pbs_job_id = self.execute_sbatch_job_and_wait(cmd)
-
-            pbs_job_array = "--dependency=afterok:$pbs_job_id"
-
-            proj_array = proj
-
-            # TODO FastQCJobs should be created from the Pipeline, rather than
-            #  within another job.
-            ### submit fastqc for current project directory
-            # fastqc_process "${seqdir}" "${output_dir}" "${fastq_output}" "${pbs_job_id}" "${pbs_job_array}" "${proj_array[@]}"
