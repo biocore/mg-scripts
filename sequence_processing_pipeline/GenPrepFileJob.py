185660d62768ee175226a2acbbae8c1e7d0b36de
Charles Cowart <42684307+charles-cowart@users.noreply.github.com>
2021-10-06 05:12:14 -0700

    Added GenPrepFileJob class (#20)
    * Added GenPrepFileJob class
    
    Added GenPrepFileJob class that wraps seqpro.
    User is expected to be running Pipeline in a python environment where metapool package has been installed.
    
    * flake8
    
    * Changes based on feedback.

--- sequence_processing_pipeline/GenPrepFileJob.py
+++ sequence_processing_pipeline/GenPrepFileJob.py
@@ -1,59 +1,48 @@
 from sequence_processing_pipeline.Job import Job
-from metapool import KLSampleSheet, validate_and_scrub_sample_sheet
 from sequence_processing_pipeline.PipelineError import PipelineError
+from os.path import join
 import logging
 
 
 class GenPrepFileJob(Job):
-    def __init__(self, run_dir, sample_sheet_path, output_directory):
+    def __init__(self, run_dir, sample_sheet_path, output_directory,
+                 seqpro_path, modules_to_load):
+        # for now, keep this run_dir instead of abspath(run_dir)
         self.job_name = 'GenPrepFileJob'
-        super().__init__(run_dir, self.job_name)
-        # metadata = self._process_sample_sheet(sample_sheet_path)
+        super().__init__(run_dir,
+                         self.job_name,
+                         [seqpro_path],
+                         modules_to_load)
+
         self.sample_sheet_path = sample_sheet_path
+        self.seqpro_path = seqpro_path
+        self.modules_to_load = modules_to_load
         self.output_directory = output_directory
 
     def run(self):
-        logging.debug('generating prep-files w/seqpro')
-        # the run directory, project name, and lane for the prep.
-        # from prep.py 424: name = run_id + '.' + project + '.' + lane
-        self._system_call(f'seqpro {self.sample_sheet_path}'
-                          f'{self.output_directory}')
+        # seqpro usage:
+        # seqpro path/to/run_dir path/to/sample/sheet /path/to/fresh/output_dir
 
-    def _process_sample_sheet(self, sample_sheet_path):
-        sheet = KLSampleSheet(sample_sheet_path)
-        valid_sheet = validate_and_scrub_sample_sheet(sheet)
+        # note that seqpro takes in a sample-sheet as input. Unlike other
+        # Jobs that process the sample-sheet, validate parameters, and ensure
+        # that output is segregated by project name, seqpro will be doing that
+        # for us. A single call to seqpro will generate n output files, one
+        # for each project described in the sample-sheet's Bioinformatics
+        # heading.
+        cmd = [self.seqpro_path, self.run_dir, self.sample_sheet_path,
+               join(self.output_directory, 'prep_files')]
 
-        if not valid_sheet:
-            s = "Sample sheet %s is not valid." % sample_sheet_path
-            raise PipelineError(s)
+        # note that if GenPrepFileJob will be run after QCJob in a Pipeline,
+        # and QCJob currently moves its products to the final location. It
+        # would be cleaner if it did not do this, but currently that is how
+        # it's done. Hence, self.output_directory and the path to run_dir
+        # might be different locations than the others.
 
-        header = valid_sheet.Header
-        chemistry = header['chemistry']
-        needs_adapter_trimming = ('TRUE' if
-                                  header['Assay'] == 'Metagenomics'
-                                  else 'FALSE')
+        out, err, rc = self._system_call(' '.join(cmd))
 
-        bioinformatics = valid_sheet.Bioinformatics
+        logging.debug(f"Seqpro stdout: {out}")
+        logging.debug(f"Seqpro stderr: {err}")
+        logging.debug(f"Seqpro return code: {rc}")
 
-        # reorganize the data into a list of dictionaries, one for each row.
-        # the ordering of the rows will be preserved in the order of the list.
-
-        lst = []
-        for i in range(0, len(bioinformatics)):
-            lst.append({})
-
-        for key in bioinformatics:
-            my_series = bioinformatics[key]
-            for index, value in my_series.items():
-                # convert all manner of positive/negative implying strings
-                # into a true boolean value.
-                if value.strip().lower() in ['true', 'yes']:
-                    value = True
-                elif value.strip().lower() in ['false', 'no']:
-                    value = False
-                lst[index][key] = value
-
-        # human-filtering jobs are scoped by project. Each job requires
-        # particular knowledge of the project.
-        return {'chemistry': chemistry, 'projects': lst,
-                'needs_adapter_trimming': needs_adapter_trimming}
+        if rc != 0:
+            raise PipelineError("Seqpro encountered an error")
