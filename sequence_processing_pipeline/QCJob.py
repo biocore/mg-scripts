41bce6733dc605594e5e0bd99c2a08d8242ff151
Antonio Gonzalez <antgonza@gmail.com>
2021-10-07 17:10:35 -0600

    final changes for job-arrays

--- sequence_processing_pipeline/QCJob.py
+++ sequence_processing_pipeline/QCJob.py
@@ -370,18 +370,6 @@ class QCJob(Job):
         # using the larger value found in the two scripts (72 vs ? hours)
         lines.append("#PBS -l walltime=%d:00:00" % wall_time_limit)
 
-        # send an email to the list of users defined below when a job starts,
-        # terminates, or aborts. This is used to confirm that the package's
-        # own reporting mechanism is reporting correctly.
-        lines.append("#PBS -m bea")
-
-        # list of users to be contacted independently of this package's
-        # notification system, when a job starts, terminates, or gets aborted.
-        lines.append("#PBS -M "
-                     # "ccowart@ucsd.edu,"
-                     # "jdereus@ucsd.edu,"
-                     "qiita.help@gmail.com")
-
         # min mem per CPU: --mem-per-cpu=<memory> -> -l pmem=<limit>
         # taking the larger of both values (10G > 6G)
         lines.append(f"#PBS -l pmem={pmem}")
@@ -390,7 +378,7 @@ class QCJob(Job):
         lines.append("#PBS -o localhost:%s.${PBS_ARRAYID}" % output_log_path)
         lines.append("#PBS -e localhost:%s.${PBS_ARRAYID}" % error_log_path)
 
-        lines.append("#PBS -t 0-%d%%20" % len(cmds))
+        lines.append("#PBS -t 1-%d%%20" % len(cmds))
 
         # there is no equivalent for this in Torque, I believe
         # --cpus-per-task 1
@@ -413,7 +401,7 @@ class QCJob(Job):
         lines.append('offset=${PBS_ARRAYID}')
         lines.append('step=$(( $offset - 0 ))')
         lines.append(f'cmd0=$(head -n $step {sh_details_fp} | tail -n 1)')
-        lines.append('cmd0')
+        lines.append('eval $cmd0')
 
         with open(job_script_path, 'w') as f:
             f.write('\n'.join(lines))
