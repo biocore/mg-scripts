2fc26e0952d3904b1e03c7de3ef6280cbe9dc2aa
Charles Cowart <ccowart@ucsd.edu>
2021-10-07 15:17:54 -0700

    Removed main()
    Removed main(), and refactored Pipeline and SequenceDirectory. They can now be used to integrate with Qiita

--- sequence_processing_pipeline/Pipeline.py
+++ sequence_processing_pipeline/Pipeline.py
@@ -4,8 +4,6 @@ from sequence_processing_pipeline.FastQCJob import FastQCJob
 from sequence_processing_pipeline.GenPrepFileJob import GenPrepFileJob
 from sequence_processing_pipeline.SequenceDirectory import SequenceDirectory
 from sequence_processing_pipeline.PipelineError import PipelineError
-from time import time as epoch_time
-import logging
 import os
 from os.path import join, exists
 import json
@@ -13,9 +11,7 @@ from json.decoder import JSONDecodeError
 
 
 class Pipeline:
-    def __init__(self, configuration_file_path, input_directory,
-                 output_directory, run_id):
-
+    def __init__(self, configuration_file_path):
         try:
             f = open(configuration_file_path)
             self.configuration = json.load(f)['configuration']
@@ -26,57 +22,18 @@ class Pipeline:
             raise PipelineError(f'{configuration_file_path} is not a valid ',
                                 'json file')
 
-        logging.debug(json.dumps(self.configuration, indent=2))
         config = self.configuration['pipeline']
-        younger_than = config['younger_than']
-        older_than = config['older_than']
-        archive_path = join(config['archive_path'], run_id)
+        self.younger_than = config['younger_than']
+        self.older_than = config['older_than']
+        self.archive_path = join(config['archive_path'])
 
-        self.run_id = run_id
+        self._directory_check(self.archive_path, create=True)
+        # self._directory_check(input_directory, create=False)
+        # self._directory_check(output_directory, create=True)
 
-        if output_directory == archive_path:
-            raise PipelineError(
-                "output_directory '%s' is the same as "
-                "archive_path '%s'." % (
-                    output_directory, archive_path))
-
-        self._directory_check(input_directory, create=False)
-        self._directory_check(output_directory, create=True)
-        self._directory_check(archive_path, create=True)
-
-        '''
-        if nprocs > 16:
-            raise PipelineError('nprocs cannot exceed 16.')
-        elif nprocs < 1:
-            raise PipelineError('nprocs cannot be less than 1.')
-        '''
-
-        if older_than >= younger_than:
+        if self.older_than >= self.younger_than:
             raise PipelineError(
                 'older_than cannot be equal to or less than younger_than.')
-
-        self.run_dir = input_directory
-
-        logging.debug("Root directory: %s" % self.run_dir)
-
-        self.sentinel_file = "RTAComplete.txt"
-        logging.debug("Sentinel File Name: %s" % self.sentinel_file)
-        # internally, threshold will be represented in seconds
-        self.younger_than = younger_than * 60 * 60
-        self.older_than = older_than * 60 * 60
-        s = "Filter directories younger than %d hours old" % younger_than
-        logging.debug(s)
-        s = "Filter directories older than %d hours old" % older_than
-        logging.debug(s)
-        # self.nprocs = nprocs
-        # logging.debug("nprocs: %d" % nprocs)
-        self.output_dir = output_directory
-        logging.debug("Output Directory: %s" % self.output_dir)
-        # location in /sequencing directory or other directory
-        # must end in name of the form 200nnn_xnnnnn_nnnn_xxxxxxxxxx or
-        # similar.
-        self.final_output_dir = archive_path
-        logging.debug("Final Output Directory: %s" % self.final_output_dir)
 
     def _directory_check(self, directory_path, create=False):
         if exists(directory_path):
@@ -94,159 +51,93 @@ class Pipeline:
                 raise PipelineError("directory_path '%s' does not exist." %
                                     directory_path)
 
-    def process(self, sample_sheet_path, qiita_job_id):
-        '''
-        Process a single BCL directory.
-        Assume sample sheet is supplied externally.
-        Assume directory doesn't require time period checking.
-        :param sample_sheet:
-        :param qiita_job_id: identify Torque jobs using qiita_job_id
-        :return:
-        '''
-        try:
-            sdo = SequenceDirectory(self.run_dir,
-                                    external_sample_sheet=sample_sheet_path)
-            sample_sheet_params = sdo.process()
 
-            ss_path = sample_sheet_params['sample_sheet_path']
-            fastq_output_directory = join(self.run_dir, 'Data', 'Fastq')
+if __name__ == '__main__':
+    import logging
 
-            # TODO: Add checks to ensure fields are defined.
-            config = self.configuration['bcl-convert']
-            convert_job = ConvertJob(self.run_dir,
-                                     ss_path,
-                                     fastq_output_directory,
-                                     config['queue'],
-                                     config['nodes'],
-                                     config['nprocs'],
-                                     config['wallclock_time_in_hours'],
-                                     config['per_process_memory_limit'],
-                                     config['executable_path'],
-                                     config['modules_to_load'],
-                                     qiita_job_id)
+    configuration_file_path = ''
+    # input_directory: A directory containing BCL files.
+    input_directory = ''
+    # A directory to store the Pipeline's products.
+    output_directory = ''
+    # identify Torque jobs using qiita_job_id
+    qiita_job_id = ''
+    # A path to the run_dir
+    run_dir = ''
+    # A path to the sample-sheet
+    sample_sheet_path = ''
+    # final_output_dir
+    final_output_dir = ''
 
-            convert_job.run()
+    # run_id='' might be useful for redefining run_id.
+    # otherwise, it will be taken from the directory.
+    sdo = SequenceDirectory(run_dir, sample_sheet_path, run_id='my_run_id')
 
-            config = self.configuration['qc']
-            qc_job = QCJob(self.run_dir,
-                           sample_sheet_params['sample_sheet_path'],
-                           config['mmi_db'],
-                           self.final_output_dir,
-                           config['queue'],
-                           config['nodes'],
+    fastq_output_directory = sdo.fastq_results_directory
+
+    pipeline = Pipeline(configuration_file_path)
+
+    config = pipeline.configuration['bcl-convert']
+    convert_job = ConvertJob(sdo.run_dir,
+                             sdo.sample_sheet_path,
+                             fastq_output_directory,
+                             config['queue'],
+                             config['nodes'],
+                             config['nprocs'],
+                             config['wallclock_time_in_hours'],
+                             config['per_process_memory_limit'],
+                             config['executable_path'],
+                             config['modules_to_load'],
+                             qiita_job_id)
+
+    convert_job.run()
+
+    config = pipeline.configuration['qc']
+    qc_job = QCJob(sdo.run_dir,
+                   sdo.sample_sheet_path,
+                   config['mmi_db'],
+                   final_output_dir,
+                   config['queue'],
+                   config['nodes'],
+                   config['nprocs'],
+                   config['wallclock_time_in_hours'],
+                   config['per_process_memory_limit'],
+                   config['fastp_executable_path'],
+                   config['minimap2_executable_path'],
+                   config['samtools_executable_path'],
+                   config['modules_to_load'],
+                   qiita_job_id)
+
+    qc_job.run()
+
+    # right now, it's preferable to use the trimmed/filtered products
+    # as they are laid out on disk the way seqpro expects. Hence the
+    # input directory for GenPrepFileJob is where QCJob moved its
+    # output files to.
+    #
+    # seqpro needs a directory named RUN_ID w/subdirectories:
+    # PROJECT_IDn/filtered_sequences or PROJECT_IDn/trimmed_sequences
+    # to generate a prep-file for that project.
+    gpf_input_path = join(final_output_dir, sdo.run_id)
+    config = pipeline.configuration['seqpro']
+    gpf_job = GenPrepFileJob(gpf_input_path,
+                             sdo.sample_sheet_path,
+                             final_output_dir,
+                             config['seqpro_path'],
+                             config['modules_to_load'],
+                             qiita_job_id)
+
+    gpf_job.run()
+
+    config = pipeline.configuration['fastqc']
+    output_directory = join(final_output_dir, 'FastQC')
+    fastqc_job = FastQCJob(sdo.run_dir,
+                           output_directory,
                            config['nprocs'],
-                           config['wallclock_time_in_hours'],
-                           config['per_process_memory_limit'],
-                           config['fastp_executable_path'],
-                           config['minimap2_executable_path'],
-                           config['samtools_executable_path'],
+                           config['nthreads'],
+                           config['fastqc_executable_path'],
+                           config['multiqc_executable_path'],
                            config['modules_to_load'],
                            qiita_job_id)
 
-            qc_job.run()
-
-            # right now, it's preferable to use the trimmed/filtered products
-            # as they are laid out on disk the way seqpro expects. Hence the
-            # input directory for GenPrepFileJob is where QCJob moved its
-            # output files to.
-            #
-            # seqpro needs a directory named RUN_ID w/subdirectories:
-            # PROJECT_IDn/filtered_sequences or PROJECT_IDn/trimmed_sequences
-            # to generate a prep-file for that project.
-            gpf_input_path = join(self.final_output_dir, self.run_id)
-            config = self.configuration['seqpro']
-            gpf_job = GenPrepFileJob(gpf_input_path,
-                                     sample_sheet_params['sample_sheet_path'],
-                                     self.final_output_dir,
-                                     config['seqpro_path'],
-                                     config['modules_to_load'],
-                                     qiita_job_id)
-
-            gpf_job.run()
-
-            config = self.configuration['fastqc']
-            output_directory = join(self.final_output_dir, 'FastQC')
-            fastqc_job = FastQCJob(self.run_dir,
-                                   output_directory,
-                                   config['nprocs'],
-                                   config['nthreads'],
-                                   config['fastqc_executable_path'],
-                                   config['multiqc_executable_path'],
-                                   config['modules_to_load'],
-                                   qiita_job_id)
-
-            fastqc_job.run()
-
-        except PipelineError as e:
-            logging.error(e)
-
-    def _time_is_right(self, timestamp):
-        # calculate how old the timestamp is
-        delta_t = epoch_time() - timestamp
-
-        # if the timestamp falls w/in the range defined by the object, then
-        # it is a legitimate target.
-        if delta_t < self.younger_than and delta_t > self.older_than:
-            return True
-
-        return False
-
-    def _find_bcl_directories(self):
-        """
-        Walk root directory and locate all scan directory root folders
-        :return: list of BCL directories within root folder.
-        """
-        new_dirs = []
-
-        for root, dirs, files in os.walk(self.run_dir):
-            for some_directory in dirs:
-                some_path = join(root, some_directory)
-                if '/Data/Intensities/BaseCalls/' in some_path:
-                    # the root directory of every scan directory will have
-                    # this substring in the path of one or more of their
-                    # subdirectories. By collecting these subdirectories
-                    # and extracting the root directory of each one, we can
-                    # build a set of unique root directories.
-                    s = some_path.split('/Data/Intensities/BaseCalls')[0]
-                    some_path = s
-                    new_dirs.append(some_path)
-
-        # remove duplicates
-        new_dirs = list(set(new_dirs))
-
-        for new_dir in new_dirs:
-            logging.info("%s found." % new_dir)
-
-        return new_dirs
-
-    def _filter_directories_for_time(self, new_dirs):
-        """
-        Filter directories for those that match allowed timespan.
-        :return: list of BCL directories within timespan.
-        """
-        filtered_dirs = []
-        for some_path in new_dirs:
-            # whether a single BCL directory is passed, or a nested tree
-            # of directories is passed, assume that a potential BCL
-            # directory must contain a file named self.sentinel_file.
-            if os.path.exists(join(some_path, self.sentinel_file)):
-                some_timestamp = os.path.getmtime(some_path)
-                # if the last modified timestamp of the directory is
-                # within the threshold of what is considered 'new and
-                # completed data',then this directory is a legitimate
-                # target.
-                if self._time_is_right(some_timestamp):
-                    # save the path as well as the original epoch timestamp
-                    # as a tuple.
-                    filtered_dirs.append((some_path, some_timestamp))
-                else:
-                    s = "The timestamp for {} is not within bounds."
-                    logging.debug(s.format(some_path))
-            else:
-                # This is a warning, rather than an error because a
-                # directory of BCL directories would be a valid parameter,
-                # even though it doesn't contain BCL data itself.
-                s = "{} does not contain a file named '{}'."
-                logging.warning(s.format(some_path, self.sentinel_file))
-
-        return filtered_dirs
+    fastqc_job.run()
--- sequence_processing_pipeline/SequenceDirectory.py
+++ sequence_processing_pipeline/SequenceDirectory.py
@@ -1,33 +1,41 @@
-import os
-import logging
-from sequence_processing_pipeline.PipelineError import PipelineError
 from metapool import KLSampleSheet, validate_and_scrub_sample_sheet
+from os.path import basename
+from sequence_processing_pipeline.PipelineError import PipelineError
+from time import time as epoch_time
+import logging
+import os
 from os.path import join, exists
 
 
 class SequenceDirectory:
-    def __init__(self, sequence_directory, external_sample_sheet):
-        if sequence_directory:
-            if exists(sequence_directory):
-                self.sequence_directory = sequence_directory
-            else:
-                s = "Directory %s does not exist." % sequence_directory
-                raise PipelineError(s)
-        else:
-            s = "A value for sequence_directory must be supplied."
-            raise PipelineError(s)
+    def __init__(self, run_dir, sample_sheet_path, run_id=None):
+        self.sentinel_file = "RTAComplete.txt"
 
-        if external_sample_sheet:
-            if exists(external_sample_sheet):
-                self.external_sample_sheet = external_sample_sheet
+        if run_dir:
+            if exists(run_dir):
+                self.run_dir = run_dir
             else:
-                s = "External sample sheet {} does not exist."
-                raise PipelineError(s.format(external_sample_sheet))
+                raise PipelineError("Directory %s does not exist." % run_dir)
         else:
-            s = "An external sample sheet must be supplied."
-            raise PipelineError(s.format(self.external_sample_sheet))
+            raise PipelineError("A value for run_dir must be supplied.")
 
-        s = join(self.sequence_directory, 'Data', 'Fastq')
+        if run_id:
+            # potentially useful for run_dirs that aren't following naming
+            # convention.
+            self.run_id = run_id
+        else:
+            self.run_id = basename(self.run_dir)
+
+        if sample_sheet_path:
+            if exists(sample_sheet_path):
+                self.sample_sheet_path = sample_sheet_path
+            else:
+                raise PipelineError(f"External sample sheet "
+                                    f"{sample_sheet_path} does not exist.")
+        else:
+            raise PipelineError("An external sample sheet must be supplied.")
+
+        s = join(self.run_dir, 'Data', 'Fastq')
         self.fastq_results_directory = s
 
         try:
@@ -38,51 +46,72 @@ class SequenceDirectory:
             # others.
             raise PipelineError(str(e))
 
-    def process(self):
-        logging.info("Processing %s..." % self.external_sample_sheet)
-
         # extract needed metadata from the sample sheet.
-        results = self._process_sample_sheet(self.external_sample_sheet)
-
-        # add to results other valuable metadata
-        results['sequence_directory'] = self.sequence_directory
-        results['sample_sheet_path'] = self.external_sample_sheet
-
-        return results
-
-    def _process_sample_sheet(self, sample_sheet):
-        results = {}
-        sheet = KLSampleSheet(sample_sheet)
+        sheet = KLSampleSheet(self.sample_sheet_path)
         valid_sheet = validate_and_scrub_sample_sheet(sheet)
 
         if not valid_sheet:
-            s = "Sample sheet %s is not valid." % sample_sheet
-            raise PipelineError(s)
+            raise PipelineError(f"Sample sheet {self.sample_sheet_path} "
+                                "is not valid.")
 
         header = valid_sheet.Header
-        experiment_name = header['Experiment Name']
-        chemistry = header['Chemistry']
+        self.experiment_name = header['Experiment Name']
+        self.chemistry = header['Chemistry']
 
-        reads = valid_sheet.Reads
-        # I7_Index_ID = valid_sheet.samples[0]['I7_Index_ID']
-        job_index_val = 'I12'
+    def find_bcl_directories(self):
+        """
+        Walk root directory and locate all scan directory root folders
+        :return: list of BCL directories within root folder.
+        """
+        new_dirs = []
 
-        if len(reads) == 1:
-            # direction == 1
-            rn1 = reads[0]
-            base_mask = "--use-bases-mask Y{},{}".format(rn1, job_index_val)
-        elif len(reads) == 2:
-            # direction == 2
-            rn1 = reads[0]
-            base_mask = "--use-bases-mask Y{},{},{},Y{}".format(rn1,
-                                                                job_index_val,
-                                                                job_index_val,
-                                                                rn1)
-        else:
-            raise PipelineError("Unexpected number of reads: %s" % str(reads))
+        for root, dirs, files in os.walk(self.run_dir):
+            for some_directory in dirs:
+                some_path = join(root, some_directory)
+                if '/Data/Intensities/BaseCalls/' in some_path:
+                    # the root directory of every scan directory will have
+                    # this substring in the path of one or more of their
+                    # subdirectories. By collecting these subdirectories
+                    # and extracting the root directory of each one, we can
+                    # build a set of unique root directories.
+                    s = some_path.split('/Data/Intensities/BaseCalls')[0]
+                    some_path = s
+                    new_dirs.append(some_path)
 
-        results['chemistry'] = chemistry
-        results['base_mask'] = base_mask
-        results['experiment_name'] = experiment_name
+        # remove duplicates
+        new_dirs = list(set(new_dirs))
 
-        return results
+        return new_dirs
+
+    def _filter_directories_for_time(self, new_dirs, younger_than, older_than):
+        """
+        Filter directories for those that match allowed timespan.
+        :return: list of BCL directories within timespan.
+        """
+        filtered_dirs = []
+        for some_path in new_dirs:
+            # whether a single BCL directory is passed, or a nested tree
+            # of directories is passed, assume that a potential BCL
+            # directory must contain a file named self.sentinel_file.
+            if os.path.exists(join(some_path, self.sentinel_file)):
+                some_timestamp = os.path.getmtime(some_path)
+                # if the last modified timestamp of the directory is
+                # within the threshold of what is considered 'new and
+                # completed data',then this directory is a legitimate
+                # target.
+                delta_t = epoch_time() - some_timestamp
+                if delta_t < younger_than and delta_t > older_than:
+                    # save the path as well as the original epoch timestamp
+                    # as a tuple.
+                    filtered_dirs.append((some_path, some_timestamp))
+                else:
+                    logging.debug(f"The timestamp for {some_path} is not"
+                                  "within bounds.")
+            else:
+                # This is a warning, rather than an error because a
+                # directory of BCL directories would be a valid parameter,
+                # even though it doesn't contain BCL data itself.
+                s = "{} does not contain a file named '{}'."
+                logging.warning(s.format(some_path, self.sentinel_file))
+
+        return filtered_dirs
