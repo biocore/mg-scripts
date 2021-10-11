b36a6e7e8b5d3281c9e9e7e91c21435827247483
Charles Cowart <ccowart@ucsd.edu>
2021-10-11 03:46:56 -0700

    flake8

--- sequence_processing_pipeline/tests/test_Pipeline.py
+++ sequence_processing_pipeline/tests/test_Pipeline.py
@@ -1,8 +1,5 @@
-from sequence_processing_pipeline.PipelineError import PipelineError
-from sequence_processing_pipeline.Pipeline import Pipeline
 import logging
 import unittest
-from os import makedirs
 
 
 logging.basicConfig(level=logging.DEBUG)
