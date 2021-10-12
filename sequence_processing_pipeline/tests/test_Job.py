a32d64bd5f5b011e8fa49625b469a981f4cec632
Antonio Gonzalez <antgonza@gmail.com>
2021-10-12 07:25:20 -0600

    add .coveragerc

--- sequence_processing_pipeline/tests/test_Job.py
+++ sequence_processing_pipeline/tests/test_Job.py
@@ -3,9 +3,6 @@ from sequence_processing_pipeline.Job import Job
 import logging
 
 
-logging.basicConfig(level=logging.DEBUG)
-
-
 class TestJob(unittest.TestCase):
     def test_system_call(self):
         job = Job('/path/to/run_dir', '200nnn_xnnnnn_nnnn_xxxxxxxxxx',
@@ -18,7 +15,6 @@ class TestJob(unittest.TestCase):
         exp_return_code = 0
         obs_std_out = results['stdout'].split('\n')
         obs_std_out = set([x.strip() for x in obs_std_out if x])
-        print(obs_std_out)
         self.assertEqual(obs_std_out, exp_stdout)
         self.assertEqual(results['stderr'], exp_stderr)
         self.assertEqual(results['return_code'], exp_return_code)
