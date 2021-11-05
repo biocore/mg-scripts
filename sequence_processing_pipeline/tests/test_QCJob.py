e9078b94a924b89876385d2009f7ecbddc75885f
Charles Cowart <ccowart@ucsd.edu>
2021-11-04 21:16:58 -0700

    Fix tests

--- sequence_processing_pipeline/tests/test_QCJob.py
+++ sequence_processing_pipeline/tests/test_QCJob.py
@@ -147,12 +147,6 @@ class TestQCJob(unittest.TestCase):
         '#PBS -V',
         '#PBS -l walltime=24:00:00',
         '#PBS -l mem=8gb',
-        '#PBS -o localhost:sequence_processing_pipeline/tests/data/output_dir/'
-        'QCJob/logs/abcdabcdabcdabcdabcdabcdabcdabcd_QCJob_NYU_BMS_Melanoma_13'
-        '059_${PBS_ARRAYID}__stdout.log',
-        '#PBS -e localhost:sequence_processing_pipeline/tests/data/output_dir/'
-        'QCJob/logs/abcdabcdabcdabcdabcdabcdabcdabcd_QCJob_NYU_BMS_Melanoma_13'
-        '059_${PBS_ARRAYID}__stderr.log',
         '#PBS -t 1-384%30',
         'set -x',
         'date',
@@ -173,12 +167,6 @@ class TestQCJob(unittest.TestCase):
         '#PBS -V',
         '#PBS -l walltime=24:00:00',
         '#PBS -l mem=8gb',
-        '#PBS -o localhost:sequence_processing_pipeline/tests/data/output_dir/'
-        'QCJob/logs/abcdabcdabcdabcdabcdabcdabcdabcd_QCJob_Feist_11661_${PBS_A'
-        'RRAYID}__stdout.log',
-        '#PBS -e localhost:sequence_processing_pipeline/tests/data/output_dir/'
-        'QCJob/logs/abcdabcdabcdabcdabcdabcdabcdabcd_QCJob_Feist_11661_${PBS_A'
-        'RRAYID}__stderr.log',
         '#PBS -t 1-432%30',
         'set -x',
         'date',
@@ -199,12 +187,6 @@ class TestQCJob(unittest.TestCase):
         '#PBS -V',
         '#PBS -l walltime=24:00:00',
         '#PBS -l mem=8gb',
-        '#PBS -o localhost:sequence_processing_pipeline/tests/data/output_dir/'
-        'QCJob/logs/abcdabcdabcdabcdabcdabcdabcdabcd_QCJob_Gerwick_6123_${PBS_'
-        'ARRAYID}__stdout.log',
-        '#PBS -e localhost:sequence_processing_pipeline/tests/data/output_dir/'
-        'QCJob/logs/abcdabcdabcdabcdabcdabcdabcdabcd_QCJob_Gerwick_6123_${PBS_'
-        'ARRAYID}__stderr.log',
         '#PBS -t 1-9%30',
         'set -x',
         'date',
