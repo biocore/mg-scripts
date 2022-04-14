641ee320b67483b3b2c070a379622ca6d7358c63
Charles Cowart <ccowart@ucsd.edu>
2022-04-14 00:11:28 -0700

    Merge QCCmdGenerator functionality into QCHelper

--- sequence_processing_pipeline/QCHelper.py
+++ sequence_processing_pipeline/QCHelper.py
@@ -1,7 +1,7 @@
 import logging
-from os.path import exists, join
-from sequence_processing_pipeline.QCCmdGenerator import QCCmdGenerator
+from os.path import exists
 from os import makedirs
+from os.path import join, split
 
 
 class QCHelper():
@@ -53,6 +53,14 @@ class QCHelper():
         if adapter_A == 'NA':
             self.adapter_A = None
 
+        if self.adapter_a is None:
+            if self.adapter_A is not None:
+                raise ValueError("adapter_a is declared but not adapter_A.")
+
+        if self.adapter_A is None:
+            if self.adapter_a is not None:
+                raise ValueError("adapter_A is declared but not adapter_a.")
+
         if not isinstance(self.a_trim, bool):
             raise ValueError("a_trim must be boolean.")
 
@@ -66,6 +74,11 @@ class QCHelper():
         # simpler to validate state.
         # possibly add checks to ensure file is valid
         self.human_phix_db_path = human_phix_db_path
+
+        if self.h_filter is True:
+            self.outdir_name = 'filtered_sequences'
+        else:
+            self.outdir_name = 'trimmed_sequences'
 
     def generate_commands(self):
         """
@@ -83,30 +96,110 @@ class QCHelper():
         cmds = []
 
         if self.a_trim is True:
-            if self.h_filter is True:
-                tmp = 'filtered_sequences'
-            else:
-                tmp = 'trimmed_sequences'
-
-            if not exists(join(self.products_dir, tmp)):
-                makedirs(join(self.products_dir, tmp), exist_ok=True)
+            if not exists(join(self.products_dir, self.outdir_name)):
+                makedirs(join(self.products_dir, self.outdir_name),
+                         exist_ok=True)
 
         if self.a_trim is True:
             for fastq_file_path in self.fastq_file_paths:
-                c = QCCmdGenerator(fastq_file_path, self.products_dir,
-                                   self.project_name, self.nprocs,
-                                   self.adapter_a,
-                                   self.adapter_A, self.fastp_path,
-                                   self.minimap2_path,
-                                   self.samtools_path)
+                current_dir = split(fastq_file_path)[0]
+                _, filename1 = split(fastq_file_path)
+                filename2 = filename1.replace('_R1_00', '_R2_00')
 
                 if self.h_filter is True:
-                    cmds.append(c.gen_chained_cmd(fastp_reports_dir,
-                                                  self.human_phix_db_path))
+                    cmds.append(self._gen_chained_cmd(current_dir,
+                                                      filename1,
+                                                      filename2,
+                                                      fastp_reports_dir))
                 else:
-                    cmds.append(c.gen_fastp_cmd())
+                    cmds.append(self._gen_fastp_cmd(current_dir,
+                                                    filename1,
+                                                    filename2))
 
             return cmds
 
         logging.warning("QCJob created w/a_trim set to False.")
         return None
+
+    def _gen_fastp_cmd(self, current_dir, filename1, filename2):
+        """
+        Generates a command-line string for running fastp, based on the
+         parameters supplied to the object.
+        :return: A string suitable for executing in Popen() and the like.
+        """
+        read1_input_path = join(current_dir, filename1)
+        read2_input_path = join(current_dir, filename2)
+
+        partial_path = join(current_dir, self.products_dir)
+        json_output_path = join(partial_path, 'json',
+                                filename1.replace('.fastq.gz', '.json'))
+        html_output_path = join(partial_path, 'html',
+                                filename1.replace('.fastq.gz', '.html'))
+        read1_output_path = join(partial_path, 'trimmed_sequences',
+                                 filename1.replace('.fastq.gz',
+                                                   '.fastp.fastq.gz'))
+        read2_output_path = join(partial_path, 'trimmed_sequences',
+                                 filename2.replace('.fastq.gz',
+                                                   '.fastp.fastq.gz'))
+        report_title = filename1.replace('.fastq.gz', '') + '_report'
+
+        result = self.fastp_path
+        if self.adapter_a:
+            # assume that if adapter_a is not None, then adapter_A is not None
+            # as well. We performed this check already.
+            result += (f' --adapter_sequence {self.adapter_a}'
+                       f' --adapter_sequence_r2 {self.adapter_A}')
+
+        result += (f' -l 100 -i {read1_input_path} -I {read2_input_path} -w '
+                   f'{self.nprocs} -j {json_output_path} -h {html_output_path}'
+                   f' -o {read1_output_path} -O {read2_output_path} -R '
+                   f'{report_title}')
+
+        return result
+
+    def _gen_chained_cmd(self, current_dir, filename1, filename2,
+                         fastp_reports_dir):
+        """
+        Generates a command-line string for running fastp piping directly
+         into minimap2 piping directly into samtools. The string is based
+         on the parameters supplied to the object.
+        :param fastp_reports_dir: Path to dir for storing json and html files
+        :param human_phix_db_path: Path to the human_phix_db_path.mmi
+        :return: A string suitable for executing in Popen() and the like.
+        """
+
+        read1_input_path = join(current_dir, filename1)
+        read2_input_path = join(current_dir, filename2)
+
+        tmp_path = join(self.project_name, fastp_reports_dir, 'json')
+        makedirs(tmp_path, exist_ok=True)
+        json_output_path = join(tmp_path, filename1.replace('.fastq.gz',
+                                                            '.json'))
+
+        tmp_path = join(self.project_name, fastp_reports_dir, 'html')
+        makedirs(tmp_path, exist_ok=True)
+        html_output_path = join(tmp_path, filename1.replace('.fastq.gz',
+                                                            '.html'))
+
+        partial = join(self.products_dir, 'filtered_sequences')
+
+        path1 = join(partial, filename1.replace('.fastq.gz',
+                                                '.trimmed.fastq.gz'))
+        path2 = join(partial, filename2.replace('.fastq.gz',
+                                                '.trimmed.fastq.gz'))
+
+        result = self.fastp_path
+        if self.adapter_a:
+            # assume that if adapter_a is not None, then adapter_A is not None
+            # as well. We performed this check already.
+            result += (f' --adapter_sequence {self.adapter_a}'
+                       f' --adapter_sequence_r2 {self.adapter_A}')
+
+        result += (f' -l 100 -i {read1_input_path} -I {read2_input_path} '
+                   f'-w {self.nprocs} -j {json_output_path} -h '
+                   f'{html_output_path} --stdout | {self.minimap2_path} -ax'
+                   f' sr -t {self.nprocs} {self.human_phix_db_path} - -a | '
+                   f'{self.samtools_path} fastq -@ {self.nprocs} -f 12 -F '
+                   f'256 -1 {path1} -2 {path2}')
+
+        return result
