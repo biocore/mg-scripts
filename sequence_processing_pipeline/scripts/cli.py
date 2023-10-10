f7c38ed2ac42ef07355cefc8107e7f5f3195682e
Daniel McDonald <danielmcdonald@ucsd.edu>
2023-10-09 21:32:50 -0700

    Removed pyproject, added cli entry point

---
+++ sequence_processing_pipeline/scripts/cli.py
@@ -0,0 +1,20 @@
+import click
+from sequence_processing_pipeline.Commands import demux
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option('--id-map', type=click.Path(exists=True), required=True)
+@click.option('--infile', type=click.Path(exists=True), required=True)
+@click.option('--output', type=click.Path(exists=True), required=True)
+@click.option('--encoded-id', type=str, required=True)
+@click.option('--threads', type=int, required=True)
+def demux(id_map, infile, output, encoded_id, threads):
+    demux(id_map, infile, output, encoded_id, threads)
+
+
+if __name__ == '__main__':
+    cli()
